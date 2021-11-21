24033b736a9500980bf2d6c6b70b44a0c206acef
liza <lizap@umich.edu>
2021-11-21 18:04:02 -0500

    add nice helpful debugging

---
+++ src/components/DailyGoals.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Daily Goals</h2>
+        <ul>
+            <li>
+                <div>
+                    <div>Water Intake: </div>
+                    <div>120 ounces</div>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <div>Time Outdoors: </div>
+                    <div>30 minutes</div>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <div>Sleep: </div>
+                    <div>8 hours</div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'DailyGoals', 
+  }
+</script>
